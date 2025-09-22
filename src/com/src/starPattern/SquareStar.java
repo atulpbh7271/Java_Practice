e19200858df6de5af7a8216f91d3226b4129af2c
atulpbh7271 <125292291+atulpbh7271@users.noreply.github.com>
2025-09-22 20:20:35 +0530

    updated the code

--- src/com/src/starPattern/SquareStar.java
+++ src/com/src/starPattern/SquareStar.java
@@ -1,4 +1,20 @@
 package com.src.starPattern;
 
+import java.util.Scanner;
+
 public class SquareStar {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++)//rows
+        {
+            for (int j = 0; j < num; j++)//columns
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
