8091c319bc7697fed9535187b1810978b466476f
atulpbh7271 <125292291+atulpbh7271@users.noreply.github.com>
2025-10-11 14:13:46 +0530

    Vowel or consonant

---
+++ src/com/src/strings/AlphabetOrNot.java
@@ -0,0 +1,7 @@
+package com.src.strings;
+
+public class AlphabetOrNot {
+    public void CheckAlphabetOrNot(char ch){
+        if(ch >= 'A' && ch <= 'Z' || )
+    }
+}
---
+++ src/com/src/strings/VowelOrConsonant.java
@@ -0,0 +1,33 @@
+package com.src.strings;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+    public void checkVowelOrConsonant(char ch) {
+
+        if (ch >= 'a' && ch <= 'z') {
+            ch = (char)(ch - 32);
+        }
+        if (ch >= 'A' && ch <= 'Z') {
+            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                System.out.println(ch + " is a vowel.");
+            } else {
+                System.out.println(ch + " is a consonant.");
+            }
+        } else {
+            System.out.println(ch + " is not an alphabet character.");
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a charater");
+        char ch = sc.next().charAt(0);
+
+       VowelOrConsonant voc = new VowelOrConsonant();
+       voc.checkVowelOrConsonant(ch);
+    }
+}
