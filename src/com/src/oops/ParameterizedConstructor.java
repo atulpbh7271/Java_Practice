da184d1b2de9b9127b681b654af4b1e7ebe07e2c
atulpbh7271 <125292291+atulpbh7271@users.noreply.github.com>
2025-10-16 17:01:13 +0530

    Parameterized constructer

--- src/com/src/oops/ParameterizedConstructor.java
+++ src/com/src/oops/ParameterizedConstructor.java
@@ -1,4 +1,23 @@
 package com.src.oops;
 
 public class ParameterizedConstructor {
+    public static class Dog {
+        private String name;
+        private String color;
+
+        Dog(String name, String color) {
+            this.name = name;
+            this.color = color;
+        }
+
+        public void displayInfo() {
+            System.out.println("Name: " + name);
+            System.out.println("Color: " + color);
+        }
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Bailey", "Black");
+        dog.displayInfo();
+    }
 }
