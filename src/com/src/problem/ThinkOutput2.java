82763b4ee7beb00291c43c19d6408137b149ae54
atulpbh7271 <125292291+atulpbh7271@users.noreply.github.com>
2025-11-14 00:00:17 +0530

    output thinking

---
+++ src/com/src/problem/ThinkOutput2.java
@@ -0,0 +1,18 @@
+package com.src.problem;
+
+public class ThinkOutput2 {
+    public static void main(String[] args){
+
+        // by default its taking double so it is not equal
+        if(0.1 + 0.2 == 0.3){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
+        System.out.println(0.1+0.2);
+        float a=0.1f;
+        float b=0.2f;
+        System.out.println(a+b);
+    }
+}
